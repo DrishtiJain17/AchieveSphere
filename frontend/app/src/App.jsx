1cf746817350789dab075d4612a5815cd09785cb
garima <garima.7.2003@gmail.com>
2025-04-16 11:08:15 +0530

    check

--- frontend/app/src/App.jsx
+++ frontend/app/src/App.jsx
@@ -5,6 +5,7 @@ function App() {
     <>
       <h1>Achieve Sphere</h1>
       <h1>Lets Fire</h1>
+      <h1>lets play</h1>
       <div>
         <input type="text" placeholder="name" />
       </div>
